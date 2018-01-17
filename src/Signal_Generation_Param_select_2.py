3b6e82dd5b4b54ae624115f2cc5130633979689f
jianqin <835559552@qq.com>
2018-01-17 20:23:19 +0800

    optimize code and add digital modulation

--- src/Signal_Generation_Param_select_2.py
+++ src/Signal_Generation_Param_select_2.py
@@ -27,7 +27,6 @@ import matplotlib
 matplotlib.use('Agg') # Must be before importing matplotlib.pyplot or pylab!
 import matplotlib.pyplot as plt
 import  random
-import  math
 
 
 learning_rate_ger = 5e-5
@@ -48,25 +47,27 @@ is_adam = False
 lam = 10.
 # max iter step, note the one step indicates that a Citers updates of critic and one update of generator
 #interval to save model
-interval_save=1000
+interval_save=2000
 #interval to save optimazaiton point
 interval_points=100
-
+#signal type
+type="AM"
 # code version
+#number of data point
+number = 32 * 8
+z_dim=128
+number=32*8
+
+#path of real data
+path = os.path.join("..", os.path.join(os.path.join("signal_data", "AM"), "am1"))
 model_name="model_WGAN"
 
 config = tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)
 config.gpu_options.allow_growth = True
 
+config.gpu_options.per_process_gpu_memory_fraction = 0.8
 
 
-
-
-#保存字符串信息
-def save(filename, contents):
-  fh = open(filename, 'w')
-  fh.write(contents)
-  fh.close()
 #lrelu activation  function
 def lrelu(x, leak=0.3, name="lrelu"):
     with tf.variable_scope(name):
@@ -77,13 +78,12 @@ def lrelu(x, leak=0.3, name="lrelu"):
 class model :
     def __init__(self, params):
         self.batch_size=params.batch_size
+        self.z_dim=params.z_dim
         self.is_mlp_g=params.is_mlp_g
         self.is_mlp_c=params.is_mlp_c
         self.max_iter_step=params.max_iter_step
         self.Noise=params.Noise
         self.loss=params.loss
-        self.type=params.type
-        self.number=params.number
 
 # read real data
     def get_signal(self,Xd, signal):
@@ -104,15 +104,12 @@ class model :
         return samples
 
     #get real data from mat file
-    def get_Signal_From(self,mat):
+    def get_Signal_From(self,mat,number):
         len=mat['s'].shape[1]
-        data = np.reshape(mat['s'][0][0:len - len % self.number].copy(), (-1, self.number))
+        data = np.reshape(mat['s'][0][0:len - len % number].copy(), (-1, number))
         indexs=np.random.randint(0,data.shape[0],self.batch_size)
         return data[indexs,:]
 
-    def get_Signal_FromData(self,data_real):
-        indexs = random.sample(range(data_real.shape[0]), self.batch_size)
-        return data_real[indexs, :]
 
 
     #相对于原始的generator_cov 的二维卷积核 该为一维卷积核
@@ -155,30 +152,38 @@ class model :
         return train5
 
 
-    def generator_mlp(self,train,base=8,recover=False):
+    def generator_mlp(self,z,size=[32,64,128],recover=False):
         noise_dist = tf.contrib.distributions.Normal(0., 1.)
         if self.Noise & (not recover):
-            noise0 = noise_dist.sample(train.get_shape().as_list())
-            train = train + noise0
-        len=int(math.log(int(self.number/base),2))
-        size=[ base * pow(2,i) for i in range(len)]
-        print('generator_mlp.size ',size)
-        train_names=[train.name]
-        for i in range(len) :
-            train= ly.fully_connected(
-               train, size[i], activation_fn=None, normalizer_fn=ly.batch_norm,scope='g{}'.format(i+1))
-            print("train_{}.name".format(i+1),train.name)
-            train_names.append(train.name)
-            if  self.Noise & (not recover):
-                noise = noise_dist.sample(train.get_shape().as_list())
-                train = train + noise
+            noise0 = noise_dist.sample(z.get_shape().as_list())
+            z = z + noise0
 
-        train1 = ly.fully_connected(
-            train, self.number, activation_fn=None, normalizer_fn=ly.batch_norm,scope='g')
+        train1= ly.fully_connected(
+            z, size[0], activation_fn=None, normalizer_fn=ly.batch_norm,scope='g1')
+        if  self.Noise & (not recover):
+            noise1 = noise_dist.sample(train1.get_shape().as_list())
+            train1 = train1 + noise1
 
-        train1= tf.reshape(train1, tf.stack([self.batch_size, 1, self.number, 1]),name="train")
-        train_names.append(train1.name)
-        return train1,train_names
+        train2 = ly.fully_connected(
+            train1, size[1], activation_fn=None, normalizer_fn=ly.batch_norm,scope='g2')
+
+        if  self.Noise & (not recover):
+            noise2= noise_dist.sample(train2.get_shape().as_list())
+            train2= train2 + noise2
+
+        train3 = ly.fully_connected(
+            train2, size[2], activation_fn=None, normalizer_fn=ly.batch_norm,scope='g3')
+        if  self.Noise & (not recover):
+            noise3 = noise_dist.sample(train3.get_shape().as_list())
+            train3 = train3+ noise3
+
+        train4 = ly.fully_connected(
+            train3, number, activation_fn=None, normalizer_fn=ly.batch_norm,scope='g4')
+
+
+        train4= tf.reshape(train4, tf.stack([self.batch_size, 1, number, 1]),name="train")
+        return train4
+
 
     #shape of  image is (1,256)
     def critic_conv(self,img, reuse=False):
@@ -198,26 +203,26 @@ class model :
                             stride=2, activation_fn=lrelu, normalizer_fn=ly.batch_norm)
             img = ly.conv2d(img, num_outputs=size * 2, kernel_size=(1,2),
                             stride=2, activation_fn=lrelu, normalizer_fn=ly.batch_norm)
+
+
             logit = ly.fully_connected(tf.reshape(
                 img, [self.batch_size, -1]), 1, activation_fn=None)
         return logit
 
-    def critic_mlp(self,img,base=16,reuse=False):
+    def critic_mlp(self,img, size=[128,64,32],reuse=False):
         noise_dist = tf.contrib.distributions.Normal(0., 1.)
-        len=int(math.log(int(self.number/base),2))
-        size=[int(self.number/pow(2,i+1)) for i in range(len)]
-        # size = [base * pow(2, i) for i in range(len)]
-        img=tf.reshape(img, [self.batch_size, -1])
-
         if self.Noise:
             noise0 = noise_dist.sample(img.get_shape().as_list())
             img = img + noise0
-
         with tf.variable_scope('critic') as scope:
             if reuse:
                 scope.reuse_variables()
-            for i in range(len):
-                img = ly.fully_connected(img, size[i], activation_fn=tf.nn.relu)
+            img = ly.fully_connected(tf.reshape(
+                img, [self.batch_size, -1]), size[0], activation_fn=tf.nn.relu)
+            img = ly.fully_connected(img, size[1],
+                                     activation_fn=tf.nn.relu)
+            img = ly.fully_connected(img, size[2],
+                                     activation_fn=tf.nn.relu)
             logit = ly.fully_connected(img, 1, activation_fn=None)
         return logit
 
@@ -246,16 +251,22 @@ class model :
         return c_loss, g_loss
 
 
+
+
+
+
     def build_graph(self):
+        #     z = tf.placeholder(tf.float32, shape=(batch_size, z_dim))
         noise_dist = tf.contrib.distributions.Normal(0., 1.)
-        z = noise_dist.sample(( self.batch_size, int(3*self.number/4)))
+        z = noise_dist.sample(( self.batch_size, self.z_dim))
         recover=tf.Variable(False,"recover")
         generator = self.generator_mlp if self.is_mlp_g else self.generator_conv
         critic =  self.critic_mlp if  self.is_mlp_c else  self.critic_conv
         with tf.variable_scope('generator'):
-            train,train_names = generator(z,recover=recover)
+            train = generator(z,recover=recover)
+        train_name=train.name
         real_data = tf.placeholder(
-            dtype=tf.float32, shape=( self.batch_size, 1, self.number, 1))
+            dtype=tf.float32, shape=( self.batch_size, 1, number, 1))
         true_logit = critic(real_data)
         fake_logit = critic(train, reuse=True)
         if self.loss=='WGAN':
@@ -280,49 +291,40 @@ class model :
                                  variables=theta_c, global_step=counter_c,
                                  summaries=['gradient_norm'])
 
-        return opt_g, opt_c, real_data,train_names
+        return opt_g, opt_c, real_data,train_name
 
 
     def train(self):
 
         tf.reset_default_graph()
         with tf.device(device):
-            opt_g, opt_c, real_data,train_names = self.build_graph()
+            opt_g, opt_c, real_data,train_name = self.build_graph()
 
+        label="G_{}_D_{}_{}_{}".format("M" if self.is_mlp_g else "C","M" if self.is_mlp_c else "C" ,self.loss,"Noise" if self.Noise else "")
+        print(label+"  " + train_name)
+        save(os.path.join('..','save',type,'log.txt'),contents=label)
 
-        path = os.path.join('..','signal_data',self.type)
+        save_path = os.path.join(os.path.join(os.path.join("..", "save"), type), label)
+        save_opt = os.path.join(save_path, "data_optimazation")
 
-        label="{}_G_{}_D_{}_{}_{}_{}_all_4".format(self.type,"M" if self.is_mlp_g else "C","M" if self.is_mlp_c else "C" ,self.loss,self.number,"Noise" if self.Noise else "")
-        print(label+"  " + str(train_names))
-       #模型保存路径和结果保存路径
-        model_save=os.path.join('..','save',self.type,label)
-        result_save=os.path.join('..','result',self.type,label)
-        if not os.path.exists(model_save):
-            os.makedirs(model_save)
+        saver = tf.train.Saver(max_to_keep=int(self.max_iter_step/interval_save))
 
-        if not os.path.exists(result_save):
-            os.makedirs(result_save)
+        # path = "D:\\gan_intro\\WGAN-tensorflow\\MNIST_data\\t10k-images.idx3-ubyte"  # the mnist data
+
+        # path =os.path.join(os.path.join("..", "signal_data"),"RML2016.10a_dict.dat")
+        # print("read real data from {} ".format(path))
+        # Xd = pickle.load(open(path, 'rb'), encoding='iso-8859-1')
 
         print("read real data from {} ".format(path))
-        data_real = np.ones((1, self.number))
-        for file in os.listdir(path):
-            if os.path.splitext(file)[1] == '.mat':
-                mat = sio.loadmat(os.path.join(path, file))
-                len = mat['s'].shape[1]
-                data = np.reshape(mat['s'][0][0:len - len % self.number].copy(), (-1, self.number))
-                print("data.shape:",data.shape)
-                data_real=np.vstack((data_real,data))
-                draw_Series_Grid(data,"real_data",result_save,"real_data_{}".format(os.path.splitext(file)[0]),row=3,col=3)
-        data_real=data_real[1:]
-        print("amount of real data to train :",data_real.shape)
-
-        saver = tf.train.Saver(max_to_keep=1+int(self.max_iter_step/interval_save))
+        mat = sio.loadmat(path)
 
 
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
 
         def next_feed_dict():
             # train_sig = get_signal(Xd, "BPSK", batch_size)
-            train_sig=np.reshape(self.get_Signal_FromData(data_real),(self.batch_size,1,self.number,1))
+            train_sig=np.reshape(self.get_Signal_From(mat,number),(self.batch_size,1,number,1))
             feed_dict = {real_data: train_sig}
             return feed_dict
         data_optima=[]
@@ -332,13 +334,14 @@ class model :
             for i in range(self.max_iter_step):
                 #save model
                 if(i%interval_save==0):
-                    saver.save(sess,os.path.join(model_save,model_name),global_step=i)
+                    saver.save(sess,os.path.join(save_path,model_name),global_step=i)
 
                 if  (i < 25 or i % 500 == 0):
                     citers = 100
                 else:
                     citers = Citers
 
+                opt_c_value_mean = np.zeros(self.batch_size, np.float16)
                 opt_c_value_mean = 0
                 for j in range(citers):
                     feed_dict = next_feed_dict()
@@ -353,7 +356,7 @@ class model :
                                                                            opt_g_value))
         # save  the the optimzation data when training this model
         data_optima=np.array(data_optima)
-        np.save(result_save,data_optima)
+        np.save(save_opt,data_optima)
         x=data_optima[:,0]
         pl1,=pl.plot(x,data_optima[:,1],"r")
         pl2,=pl.plot(x,data_optima[:,2],"g")
@@ -361,80 +364,86 @@ class model :
         pl.ylabel("loss")
         pl.title("optimazation process for {}".format(label))
         pl.legend(handles=[pl1,pl2],labels=['opt_c_value_mean','opt_g_value'])
-        pl.savefig(os.path.join(result_save,"pic_optima"))
+        pl.savefig(os.path.join(save_path,"pic_optima"))
         sess.close()
-        return  train_names,self.type,label,self.number,model_save,result_save
+        return  train_name,label
 
-def oneSolution(batch_size,is_mlp_g,is_mlp_c,max_iter_step,Noise,loss,type,number):
+def oneSolution(batch_size,z_dim,is_mlp_g,is_mlp_c,max_iter_step,Noise,loss):
     parser = argparse.ArgumentParser()
     parser.add_argument('--batch-size', type=int, default=batch_size,
                         help='the size of one epoch')
+    parser.add_argument('--z-dim',type=int,default=z_dim)
     parser.add_argument('--is-mlp-g', type=bool, default=is_mlp_g)
     parser.add_argument('--is-mlp-c', type=bool, default=is_mlp_c)
     parser.add_argument('--max-iter-step',type=int,default=max_iter_step)
     parser.add_argument('--Noise', type=bool, default=Noise)
     parser.add_argument('--loss', type=str, default=loss)
-    parser.add_argument('--type', type=str, default=type)
-    parser.add_argument('--number', type=int, default=number)
     return model(parser.parse_args()).train()
 
 
 
 def searchParam():    # main()
     # batch_size_=[16,32,64,128,256]
-    # real_data_ = ['AM', 'DSB', 'USB', 'LSB', 'FM']
-    real_data_=['AM','DSB']
-    number_=[512,1024]
-    for real in real_data_:
-        for num in number_:
-            number=num
-            real_data=real
-            loss='WGAN'
-            is_mlp_g=True
-            batch_size=128
-            is_mlp_c=True
-            max_iter_step=5001
-            Noise=True
-            train_names,type, label,number, model_save, result_save=oneSolution(batch_size,is_mlp_g,is_mlp_c,max_iter_step,Noise,loss,real_data,number)
-            #恢复数据`
-            recover_model(model_save,train_names,result_save,type, label,model_interval=interval_save,model_number=1+int(max_iter_step/interval_save))
+    batch_size_=[128]
+    z_dim_=[128]
+    loss_=['WGAN','LSGAN']
+    is_mlp_c_=[True]
+    is_mlp_g_=[True,False]
+    Noise_=[True,False]
+    max_iter_step_=[20001]
+
+    for l in loss_:
+        loss=l
+        for z in z_dim_:
+            z_dim = z
+            for c in is_mlp_c_:
+                is_mlp_c = c
+                for g in is_mlp_g_:
+                    is_mlp_g=g
+                    for max in max_iter_step_:
+                        max_iter_step = max
+                        for batch_ in batch_size_:
+                            for no in Noise_:
+                                Noise=no
+                                batch_size=batch_
+                                train_name,label=oneSolution(batch_size,z_dim,is_mlp_g,is_mlp_c,max_iter_step,Noise,loss)
+                                #恢复数据
+                                recover_model(label,model_interval=interval_save,model_number=int(max_iter_step/interval_save),train_name=train_name)
 
 
 
-def draw_Series_Grid(seq,title,save_to,name,row,col):
+def draw_Series_Grid(seq,title,save_to,name,row,col,N=64):
    fig ,axes=plt.subplots(row,col,sharex=True,sharey=True)
    if not os.path.exists(save_to):
        os.makedirs(save_to)
    for i in range(row):
        for j in range(col):
-           axes[i,j].plot(range(seq.shape[1]),seq[i+j][:],'.-')
+           index=random.sample(range(seq.shape[0]),1)[0]
+           print("get data at index {}".format(index))
+           axes[i,j].plot(range(N),seq[index][:N],'.-')
    fig.suptitle(title)
+   print('save to {}'.format(save_to))
    plt.savefig(os.path.join(save_to,name))
    fig.clear()
 
-def recover_pic(model_save,result_save,type,label,tensorname,pic_number=2, iterations = 16000):
+def recover_pic(label,type="AM",pic_number=2, iterations = 16000,tensorname="generator/train:0",seq_number=64):
     print("regenerate data at {} {} {} ".format(type,label,iterations))
-
-    model_path=os.path.join(model_save,"{}-{}".format('model_WGAN',iterations))
-    saver = tf.train.import_meta_graph(model_path+ ".meta")
+    path = os.path.join("..","save",type,label,"{}-{}".format("model_WGAN",iterations))
+    save_to =os.path.join("..","result",type,label)
+    saver = tf.train.import_meta_graph(path + ".meta")
+    print("restore model from {}".format(path))
     with tf.Session(config=config) as sess:
-        saver.restore(sess, model_path)
+        saver.restore(sess, path)
         recover=tf.get_default_graph().get_tensor_by_name("Variable:0")
         dict={recover:True}
-
-        print("restore model from {} to regenerate data {}".format(model_path,str(tensorname)))
-        train_names=[tf.get_default_graph().get_tensor_by_name(tensorname[i]) for i in range(len(tensorname))]
-        train_data_all = sess.run(train_names,dict)
-        indexs=random.sample(range(train_data_all[0].shape[0]),pic_number*pic_number)
-        for i in range(len(tensorname)):
-            train = np.array(train_data_all[i]).reshape((train_data_all[i].shape[0],-1))
-            title=" {}_signal on model_{} layer_{} iter_{}".format(type,label,i,iterations)
-            draw_Series_Grid(train[indexs],title,result_save,name="signal_{}_layer_{}".format(iterations,i),row=pic_number,col=pic_number)
+        train = sess.run(tf.get_default_graph().get_tensor_by_name(tensorname),dict)
+        train = np.array(train).reshape((-1, number))
+        draw_Series_Grid(train," {}_signal on model_{} iter_{}".format(type,label,iterations),
+                         save_to,name="signal_{}".format(iterations),row=pic_number,col=pic_number,N=seq_number)
 
 
-def recover_model(model_save,train_names,result_save,type,label,model_number = 14,model_interval = 3000,
-                  pic_number=2):
+def recover_model(label,model_number = 14,model_interval = 3000,
+                  pic_number=4,seq_number = 64 ,train_name="generator/g4/Tanh:0"):
     for i in range(model_number):
-        recover_pic(model_save,result_save,type,label, iterations=i* model_interval,pic_number=pic_number,tensorname=train_names)
-
+        recover_pic(label, iterations=(i +1)* model_interval,pic_number=pic_number,tensorname=train_name,seq_number=seq_number)
 searchParam()
